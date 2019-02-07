0359038044d7c2bd6ec590f3ba0506e6c3b827a6
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-02-07 11:42:50 -0700

    added images and friend.js array

---
+++ app/data/friends.js
@@ -0,0 +1,168 @@
+// DATA
+// Holds array of objects detailing individual dating profiles
+
+const profilesArray = [
+    {
+        name: 'Dorian',
+        photo: '/images/pexels-ma.jpeg',
+        scores: [
+            4,
+            2,
+            2,
+            2,
+            4,
+            3,
+            5,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: 'Rowan',
+        photo: '/images/pexels-fna.jpeg',
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: 'Cameron',
+        photo: '/images/pexels-mb.jpeg',
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: 'Morgan',
+        photo: '/images/pexels-fb.jpeg',
+        scores: [
+            4,
+            5,
+            2,
+            2,
+            3,
+            1,
+            5,
+            3,
+            2,
+            2
+        ]
+    },
+    {
+        name: 'Sydney',
+        photo: '/images/pexels-mi.jpeg',
+        scores: [
+            2,
+            4,
+            5,
+            3,
+            1,
+            2,
+            5,
+            3,
+            4,
+            2
+        ]
+    },
+    {
+        name: 'Aubrey',
+        photo: '/images/pexels-fa.jpeg',
+        scores: [
+            4,
+            4,
+            4,
+            4,
+            2,
+            1,
+            4,
+            4,
+            3,
+            5
+        ]
+    },
+    {
+        name: 'Finley',
+        photo: '/images/pexels-fw.jpeg',
+        scores: [
+            3,
+            2,
+            3,
+            2,
+            1,
+            1,
+            1,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        name: 'Hayden',
+        photo: '/images/pexels-mw.jpeg',
+        scores: [
+            5,
+            2,
+            1,
+            1,
+            3,
+            4,
+            4,
+            2,
+            1,
+            3            
+        ]
+    },
+    {
+        name: 'Kai',
+        photo: '/images/pexels-c.jpeg',
+        scores: [
+           1,
+           1,
+           1,
+           1,
+           1,
+           1,
+           1,
+           1,
+           1,
+           1 
+        ]
+    },
+    {
+        name: 'London',
+        photo: '/images/pexels-mwy.jpeg',
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    }
+];
+
+// Exporting the array to be used in other files
+module.exports = profilesArray;
