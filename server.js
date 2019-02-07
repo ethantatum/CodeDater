b9959fe742a511b6bec6633344824f994d206606
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-02-06 18:11:14 -0700

    added dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+// DEPENDENCIES
+const express = require(`express`);
+const path = require(`path`);
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
