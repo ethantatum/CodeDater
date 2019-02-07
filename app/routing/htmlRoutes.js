28713b0cfc20ce54173320b384eb9e05ef9432b3
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-02-07 13:38:36 -0700

    completed server.js file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,18 @@
+// Dependencies
+
+const path = require('path');
+
+// Routing
+// Directs users to different pages based on what the URI is
+
+module.exports = function(app) {
+
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, `../public/home.html`));
+    });
+
+};
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-// DEPENDENCIES
+// Dependencies
 const express = require(`express`);
-const path = require(`path`);
 
+// Creates express server
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Express middleware for parsing data
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+// Router
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Listener
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}
+    `);
+});
